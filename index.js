058f217f14665506e78d80a5c0e5d4cd1953b08c
Karan800 <nagurekaran@gmail.com>
2022-06-02 16:55:45 +0530

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Yo");
+const express = require("express")
+
+const app = express()
+
+app.get("/user",(req,res)=>{
+
+    res.send("wc to den")
+})
+
+const port = 7000
+app.listen(port,(req,res)=>{
+
+    console.log(`Starting my APP on:${port}`);
+})
